bd9c7ab5bc54b2919e04fe38e4d1b72a18b54f2b
Ian Garnett <designpxls@gmail.com>
2022-12-18 23:30:32 -0500

    updated file path

--- js/repoData.js
+++ js/repoData.js
@@ -17,291 +17,291 @@ export const data = [
    },
    {
     imgName: 'brit',
-    imgSrc: '/img/imgsmall/brit-flood-min.jpg',
+    imgSrc: './img/imgsmall/brit-flood-min.jpg',
   },
   {
     imgName: 'william',
-    imgSrc: '/img/imgsmall/william-flood-min.jpg',
+    imgSrc: './img/imgsmall/william-flood-min.jpg',
   },
   {
     imgName: 'tramesha',
-    imgSrc: '/img/imgsmall/tramesha-flood-min.jpg',
+    imgSrc: './img/imgsmall/tramesha-flood-min.jpg',
   },
   {
     imgName: 'kayla',
-    imgSrc: '/img/imgsmall/kayla-veloz-min.jpg',
+    imgSrc: './img/imgsmall/kayla-veloz-min.jpg',
   },  
   {
     imgName: 'riley',
-    imgSrc: '/img/imgsmall/riley-flood-min.jpg',
+    imgSrc: './img/imgsmall/riley-flood-min.jpg',
   },
   {
     imgName: 'chalie',
-    imgSrc: '/img/imgsmall/chalie-flood-min.jpg',
+    imgSrc: './img/imgsmall/chalie-flood-min.jpg',
   },
   {
     imgName: 'cameron',
-    imgSrc: '/img/imgsmall/cameron-flood-min.jpg',
+    imgSrc: './img/imgsmall/cameron-flood-min.jpg',
   },
   {
     imgName: 'gail',
-    imgSrc: '/img/imgsmall/gail-minor-min.jpg',
+    imgSrc: './img/imgsmall/gail-minor-min.jpg',
   },
   {
     imgName: 'michael',
-    imgSrc: '/img/imgsmall/michael-minor-min.jpg',
+    imgSrc: './img/imgsmall/michael-minor-min.jpg',
   },
   {
     imgName: 'leanda',
-    imgSrc: '/img/imgsmall/leanda-feaster-min.jpg',
+    imgSrc: './img/imgsmall/leanda-feaster-min.jpg',
   },
   {
     imgName: 'shawn',
-    imgSrc: '/img/imgsmall/shawn-taylor-min.jpg',
+    imgSrc: './img/imgsmall/shawn-taylor-min.jpg',
   },
   {
     imgName: 'edward',
-    imgSrc: '/img/imgsmall/edward-taylor-min.jpg',
+    imgSrc: './img/imgsmall/edward-taylor-min.jpg',
   },
   {
     imgName: 'reggie',
-    imgSrc: '/img/imgsmall/reggie-taylor-min.jpg',
+    imgSrc: './img/imgsmall/reggie-taylor-min.jpg',
   },
   {
     imgName: 'brandon',
-    imgSrc: '/img/imgsmall/brandon-taylor-min.jpg',
+    imgSrc: './img/imgsmall/brandon-taylor-min.jpg',
   },
   {
     imgName: 'jenna',
-    imgSrc: '/img/imgsmall/jenna-taylor-min.jpg',
+    imgSrc: './img/imgsmall/jenna-taylor-min.jpg',
   },
   {
     imgName: 'eric',
-    imgSrc: '/img/imgsmall/eric-armstead-min.jpg',
+    imgSrc: './img/imgsmall/eric-armstead-min.jpg',
   },
   {
     imgName: 'katrina',
-    imgSrc: '/img/imgsmall/katrina-armstead-min.jpg',
+    imgSrc: './img/imgsmall/katrina-armstead-min.jpg',
   },
   {
     imgName: 'ericka',
-    imgSrc: '/img/imgsmall/ericka-armsted-min.jpg',
+    imgSrc: './img/imgsmall/ericka-armsted-min.jpg',
   },
   {
     imgName: 'maya',
-    imgSrc: '/img/imgsmall/maya-armstead-min.jpg',
+    imgSrc: './img/imgsmall/maya-armstead-min.jpg',
   },
   {
     imgName: 'mykel',
-    imgSrc: '/img/imgsmall/mykel-armstead-min.jpg',
+    imgSrc: './img/imgsmall/mykel-armstead-min.jpg',
   },
   {
     imgName: 'marlene',
-    imgSrc: '/img/imgsmall/marlene-bailey-min.jpg',
+    imgSrc: './img/imgsmall/marlene-bailey-min.jpg',
   },
   {
     imgName: 'darlene',
-    imgSrc: '/img/imgsmall/darlene-cook-min.jpg',
+    imgSrc: './img/imgsmall/darlene-cook-min.jpg',
   },
   {
     imgName: 'kj',
-    imgSrc: '/img/imgsmall/kj-armstead-min.jpg',
+    imgSrc: './img/imgsmall/kj-armstead-min.jpg',
   },
   {
     imgName: 'kristian',
-    imgSrc: '/img/imgsmall/kristian-roman-min.jpg',
+    imgSrc: './img/imgsmall/kristian-roman-min.jpg',
   },
   {
     imgName: 'charles',
-    imgSrc: '/img/imgsmall/charles-garnett-min.jpg',
+    imgSrc: './img/imgsmall/charles-garnett-min.jpg',
   },
   {
     imgName: 'audrey',
-    imgSrc: '/img/imgsmall/audrey-garnett-min.jpg',
+    imgSrc: './img/imgsmall/audrey-garnett-min.jpg',
   },
   {
     imgName: 'Charles',
-    imgSrc: '/img/imgsmall/chuck-garnett-min.jpeg',
+    imgSrc: './img/imgsmall/chuck-garnett-min.jpeg',
   },
   {
     imgName: 'melissa',
-    imgSrc: '/img/imgsmall/melissa-garnett-min.jpg',
+    imgSrc: './img/imgsmall/melissa-garnett-min.jpg',
   },
   {
     imgName: 'ian',
-    imgSrc: '/img/imgsmall/ian-garnett-min.jpg',
+    imgSrc: './img/imgsmall/ian-garnett-min.jpg',
   }, 
   {
     imgName: 'quin',
-    imgSrc: '/img/imgsmall/quin-freeman-min.jpeg',
+    imgSrc: './img/imgsmall/quin-freeman-min.jpeg',
   },
   {
     imgName: 'remelda',
-    imgSrc: '/img/imgsmall/remelda-jackson-min.jpg',
+    imgSrc: './img/imgsmall/remelda-jackson-min.jpg',
   },
   {
     imgName: 'jeff',
-    imgSrc: '/img/imgsmall/jeff-rollins-min.jpg',
+    imgSrc: './img/imgsmall/jeff-rollins-min.jpg',
   },
   {
     imgName: 'kotonya',
-    imgSrc: '/img/imgsmall/kotonya-rollins-min.jpg',
+    imgSrc: './img/imgsmall/kotonya-rollins-min.jpg',
   },
   {
     imgName: 'kianna',
-    imgSrc: '/img/imgsmall/kianna-rollins-min.jpg',
+    imgSrc: './img/imgsmall/kianna-rollins-min.jpg',
   },
   {
     imgName: 'stephanie',
-    imgSrc: '/img/imgsmall/stephanie-rollins-min.jpg',
+    imgSrc: './img/imgsmall/stephanie-rollins-min.jpg',
   },
   {
     imgName: 'torin',
-    imgSrc: '/img/imgsmall/torin-rollins-min.jpg',
+    imgSrc: './img/imgsmall/torin-rollins-min.jpg',
   },
   {
     imgName: 'jaxon',
-    imgSrc: '/img/imgsmall/jaxon-rollins-min.jpg',
+    imgSrc: './img/imgsmall/jaxon-rollins-min.jpg',
   },
   {
     imgName: 'toriana',
-    imgSrc: '/img/imgsmall/toriana-rollins-min.jpg',
+    imgSrc: './img/imgsmall/toriana-rollins-min.jpg',
   },
   {
     imgName: 'erin',
-    imgSrc: '/img/imgsmall/erin-rollins-min.jpg',
+    imgSrc: './img/imgsmall/erin-rollins-min.jpg',
   },
   {
     imgName: 'donna',
-    imgSrc: '/img/imgsmall/donna-stephens-min.jpg',
+    imgSrc: './img/imgsmall/donna-stephens-min.jpg',
   },
   {
     imgName: 'ronnel',
-    imgSrc: '/img/imgsmall/ronnel-coghill-min.jpg',
+    imgSrc: './img/imgsmall/ronnel-coghill-min.jpg',
   },
   {
     imgName: 'lauryn',
-    imgSrc: '/img/imgsmall/lauryn-wilson-min.jpg',
+    imgSrc: './img/imgsmall/lauryn-wilson-min.jpg',
   },
   {
     imgName: 'aubrey',
-    imgSrc: '/img/imgsmall/aubrey-wilson-min.jpg',
+    imgSrc: './img/imgsmall/aubrey-wilson-min.jpg',
   },
   {
     imgName: 'kay',
-    imgSrc: '/img/imgsmall/kay-bell-min.jpg',
+    imgSrc: './img/imgsmall/kay-bell-min.jpg',
   },
   {
     imgName: 'greg',
-    imgSrc: '/img/imgsmall/greg-bell-min.jpg',
+    imgSrc: './img/imgsmall/greg-bell-min.jpg',
   },
   {
     imgName: 'tiffany',
-    imgSrc: '/img/imgsmall/tiffany-bell-min.jpg',
+    imgSrc: './img/imgsmall/tiffany-bell-min.jpg',
   },
   {
     imgName: 'nicole',
-    imgSrc: '/img/imgsmall/nicole-bell-min.jpg',
+    imgSrc: './img/imgsmall/nicole-bell-min.jpg',
   },
   {
     imgName: 'jackie',
-    imgSrc: '/img/imgsmall/jackie-welch-min.jpg',
+    imgSrc: './img/imgsmall/jackie-welch-min.jpg',
   },
   {
     imgName: 'bryant',
-    imgSrc: '/img/imgsmall/bryant-bell-min.jpg',
+    imgSrc: './img/imgsmall/bryant-bell-min.jpg',
   },
   {
     imgName: 'stephanie',
-    imgSrc: '/img/imgsmall/stephanie-bell-min.jpg',
+    imgSrc: './img/imgsmall/stephanie-bell-min.jpg',
   },
   {
     imgName: 'caitlin',
-    imgSrc: '/img/imgsmall/caitlin-bell-min.jpg',
+    imgSrc: './img/imgsmall/caitlin-bell-min.jpg',
   },
   {
     imgName: 'Boo',
-    imgSrc: '/img/imgsmall/boo-min.jpg',
+    imgSrc: './img/imgsmall/boo-min.jpg',
   },
   {
     imgName: 'margaet',
-    imgSrc: '/img/imgsmall/margaet-hale-min.jpg',
+    imgSrc: './img/imgsmall/margaet-hale-min.jpg',
   },
   {
     imgName: 'Diaunte',
-    imgSrc: '/img/imgsmall/Diaunte-bell-min.jpg',
+    imgSrc: './img/imgsmall/Diaunte-bell-min.jpg',
   },
   {
     imgName: 'josh',
-    imgSrc: '/img/imgsmall/josh-hale-min.jpg',
+    imgSrc: './img/imgsmall/josh-hale-min.jpg',
   },
   {
     imgName: 'angela',
-    imgSrc: '/img/imgsmall/angela-web-min.jpg',
+    imgSrc: './img/imgsmall/angela-web-min.jpg',
   },
   {
     imgName: 'Devin',
-    imgSrc: '/img/imgsmall/djordan-min.jpg',
+    imgSrc: './img/imgsmall/djordan-min.jpg',
   },
   {
     imgName: 'tierra',
-    imgSrc: '/img/imgsmall/tierra-jordan-min.jpg',
+    imgSrc: './img/imgsmall/tierra-jordan-min.jpg',
   },
   {
     imgName: 'elijah',
-    imgSrc: '/img/imgsmall/elijah-kay-min.jpg',
+    imgSrc: './img/imgsmall/elijah-kay-min.jpg',
   },
   {
     imgName: 'deshawn',
-    imgSrc: '/img/imgsmall/deshawn-jordan-min.jpg',
+    imgSrc: './img/imgsmall/deshawn-jordan-min.jpg',
   },
   {
     imgName: 'niecy',
-    imgSrc: '/img/imgsmall/niecy-jackson-min.jpg',
+    imgSrc: './img/imgsmall/niecy-jackson-min.jpg',
   },
   {
     imgName: 'remelda',
-    imgSrc: '/img/imgsmall/remelda-jackson-min.jpg',
+    imgSrc: './img/imgsmall/remelda-jackson-min.jpg',
   },
   {
     imgName: 'janice',
-    imgSrc: '/img/imgsmall/janice-lucas-min.jpg',
+    imgSrc: './img/imgsmall/janice-lucas-min.jpg',
   },
   {
     imgName: 'gail',
-    imgSrc: '/img/imgsmall/gail-minor-min.jpg',
+    imgSrc: './img/imgsmall/gail-minor-min.jpg',
   },
   {
     imgName: 'michael',
-    imgSrc: '/img/imgsmall/michael-minor-min.jpg',
+    imgSrc: './img/imgsmall/michael-minor-min.jpg',
   },
   {
     imgName: 'alyssa',
-    imgSrc: '/img/imgsmall/alyssa-renee-min.jpg',
+    imgSrc: './img/imgsmall/alyssa-renee-min.jpg',
   },
   {
     imgName: 'angela',
-    imgSrc: '/img/imgsmall/angela-washington-min.jpg',
+    imgSrc: './img/imgsmall/angela-washington-min.jpg',
   },
   {
     imgName: 'darius',
-    imgSrc: '/img/imgsmall/darius-wash-min.jpg',
+    imgSrc: './img/imgsmall/darius-wash-min.jpg',
   },
   {
     imgName: 'jai',
-    imgSrc: '/img/imgsmall/jai-franklin-min.jpg',
+    imgSrc: './img/imgsmall/jai-franklin-min.jpg',
   },
   {
     imgName: 'terri',
-    imgSrc: '/img/imgsmall/terri-eiland-min.jpg',
+    imgSrc: './img/imgsmall/terri-eiland-min.jpg',
   },
   {
     imgName: 'sylenna',
-    imgSrc: '/img/imgsmall/sylenna-byrd-min.jpg',
+    imgSrc: './img/imgsmall/sylenna-byrd-min.jpg',
   },
   {
     imgName: 'lauren',
-    imgSrc: '/img/imgsmall/lauren-michelle-min.jpg',
+    imgSrc: './img/imgsmall/lauren-michelle-min.jpg',
   }
 
 
